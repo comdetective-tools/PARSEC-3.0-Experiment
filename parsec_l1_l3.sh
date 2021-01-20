5b943cbaf0f504b630d3c35e3065b8df20d3e560
msasongko17 <msasongko17@ku.edu.tr>
2021-01-20 14:29:34 +0300

    updated scripts

---
+++ parsec_l1_l3.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+./blackscholes_l1_run_cacheline_inv_gold.sh
+./blackscholes_l1_run_cacheline_inv_gold_spatial.sh
+./bodytrack_l1_run_cacheline_inv_gold.sh
+./bodytrack_l1_run_cacheline_inv_gold_spatial.sh
+./streamcluster_l1_run_cacheline_inv_gold.sh
+./streamcluster_l1_run_cacheline_inv_gold_spatial.sh
+./freqmine_l1_run_cacheline_inv_gold.sh
+./freqmine_l1_run_cacheline_inv_gold_spatial.sh
